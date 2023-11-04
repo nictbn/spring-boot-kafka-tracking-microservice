e36f3a1b055f0ebbfc6fe8290f82a197c7e8f915
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-04 17:47:41 +0200

    Adds integration test

--- src/main/java/com/example/tracking/TrackingConfiguration.java
+++ src/main/java/com/example/tracking/TrackingConfiguration.java
@@ -36,7 +36,6 @@ public class TrackingConfiguration {
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, DispatchPreparing.class.getCanonicalName());
-        config.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
     }
@@ -52,7 +51,6 @@ public class TrackingConfiguration {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         return new DefaultKafkaProducerFactory<>(config);
     }
 
---
+++ src/test/java/com/example/tracking/integration/TrackingIntegrationTest.java
@@ -0,0 +1,95 @@
+package com.example.tracking.integration;
+
+import com.example.tracking.TrackingConfiguration;
+import com.example.tracking.message.DispatchPreparing;
+import com.example.tracking.message.TrackingStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.example.tracking.util.TestEventData.buildDispatchPreparingEvent;
+import static java.util.UUID.randomUUID;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+@SpringBootTest(classes = {TrackingConfiguration.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+@EmbeddedKafka(controlledShutdown = true)
+public class TrackingIntegrationTest {
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+
+    public static final String TRACKING_STATUS_TOPIC = "tracking.status";
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry registry;
+
+    @Autowired
+    private KafkaTestListener testListener;
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public KafkaTestListener testListener() {
+            return new KafkaTestListener();
+        }
+    }
+
+    public static class KafkaTestListener {
+        AtomicInteger trackingStatusMessageCounter = new AtomicInteger(0);
+        @KafkaListener(groupId = "kafkaIntegrationTest", topics = TRACKING_STATUS_TOPIC)
+        void receiveTrackingStatusMessage(@Payload TrackingStatus payload) {
+            log.debug("Received TrackingStatus: " + payload);
+            trackingStatusMessageCounter.incrementAndGet();
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        testListener.trackingStatusMessageCounter.set(0);
+        registry.getListenerContainers().stream().forEach(container ->
+                ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic()));
+    }
+
+    @Test
+    public void testFlow() throws Exception {
+        DispatchPreparing orderCreated = buildDispatchPreparingEvent(randomUUID());
+        sendMessage(DISPATCH_TRACKING_TOPIC, orderCreated);
+        await()
+                .atMost(3, TimeUnit.SECONDS)
+                .pollDelay(100, TimeUnit.MILLISECONDS)
+                .until(testListener.trackingStatusMessageCounter::get, equalTo(1));
+    }
+
+    private void sendMessage(String topic, Object data) throws Exception {
+        kafkaTemplate.send(MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build()).get();
+    }
+}
