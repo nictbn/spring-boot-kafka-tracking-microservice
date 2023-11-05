829810ff75a23a2ec0023259e56a0d04b591bf20
Nicu Taban <nictbndeveloper@gmail.com>
2023-11-05 12:53:36 +0200

    Moves the KafkaListener annotation to the class level and adds the KafkaHandler annotation at the method level

--- src/main/java/com/example/tracking/handler/TrackingHandler.java
+++ src/main/java/com/example/tracking/handler/TrackingHandler.java
@@ -4,20 +4,22 @@ import com.example.tracking.message.DispatchPreparing;
 import com.example.tracking.service.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
+@KafkaListener(
+        id = "trackingConsumerClient",
+        topics = "dispatch.tracking",
+        groupId = "dispatch.tracking.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class TrackingHandler {
     private final TrackingService trackingService;
-    @KafkaListener(
-            id = "trackingConsumerClient",
-            topics = "dispatch.tracking",
-            groupId = "dispatch.tracking.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparing payload) {
         log.info("Received message payload: " + payload);
         try {
